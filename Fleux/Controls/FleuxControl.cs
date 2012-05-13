d4f5a9e5ccb15a196d9949c78158f6b8e0d514a0
cail <igor@hobdrive.com>
2012-05-13 06:38:19 +0400

    possible exception in Draw

--- Fleux/Controls/FleuxControl.cs
+++ Fleux/Controls/FleuxControl.cs
@@ -207,7 +207,13 @@
             {
                 e.Graphics.Clear(this.SafeBackColor);
                 var gr = DrawingGraphics.FromGraphicsAndRect(this.offGr, this.offBmp, new Rectangle(0, 0, this.offBmp.Width, this.offBmp.Height));
-                this.elements.ForEach(element => element.Draw(gr.CreateChild(element.Location, element.TransformationScaling, element.TransformationCenter)));
+                try{
+                    this.elements.ForEach(element => element.Draw(gr.CreateChild(element.Location, element.TransformationScaling, element.TransformationCenter)));
+                }catch(Exception){
+                  // TODO drawing exception because of
+                  //     System.InvalidOperationException: Collection was modified;
+                  // on elements
+                }
                 if (this.ShadowedAnimationMode != ShadowedAnimationOptions.None
                     && this.shadowImageX < this.offBmp.Width
                     && this.shadowImageX > -this.offBmp.Width * 3)
