384f7adeb9c01008f2b2eebb87b2a1ad310e9982
Igor Russkikh <igor@hobdrive.com>
2026-01-10 23:20:31 +0100

    better rectangle draw

--- Fleux/Core/GraphicsHelpers/GraphicsExtensions.cs
+++ Fleux/Core/GraphicsHelpers/GraphicsExtensions.cs
@@ -64,24 +64,24 @@ namespace Fleux.Core.GraphicsHelpers
         /// <summary>
         /// Fills the rounded rectangle with roundings in "roundings"
         /// </summary>
-        public static IDrawingGraphics DrawRoundedRectangle(this IDrawingGraphics dg, System.Drawing.Rectangle rect, int l, int r, int lb, int rb)
+        public static IDrawingGraphics DrawRoundedRectangle(this IDrawingGraphics dg, System.Drawing.Rectangle rect, int lt, int rt, int lb, int rb)
         {
             int ix, iy, ix2, iy2;
-            ix = rect.X + Math.Max(l, lb);
-            iy = rect.Y + Math.Max(l, r);
-            ix2 = rect.Right - Math.Max(r, rb);
+            ix = rect.X + Math.Max(lt, lb);
+            iy = rect.Y + Math.Max(lt, rt);
+            ix2 = rect.Right - Math.Max(rt, rb);
             iy2 = rect.Bottom - Math.Max(lb, rb);
 
-            dg.DrawArc(rect.X, rect.Y, ix+l, iy+l, (float)Math.PI, (float)Math.PI/2);
+            dg.DrawArc(rect.X, rect.Y, ix+lt, iy+lt, (float)Math.PI, (float)Math.PI/2);
             dg.DrawArc(rect.X, rect.Bottom, ix+lb, iy2-lb, (float)Math.PI, -(float)Math.PI/2);
-            dg.DrawArc(ix2-r, rect.Y, rect.Right, iy+r, (float)0, -(float)Math.PI/2);
+            dg.DrawArc(ix2-rt, rect.Y, rect.Right, iy+rt, (float)0, -(float)Math.PI/2);
             dg.DrawArc(rect.Right, rect.Bottom, ix2-rb, iy2-rb, 0, (float)Math.PI/2);
 
             dg.DrawLine(ix, rect.Y, ix2, rect.Y);
             dg.DrawLine(ix, rect.Bottom, ix2, rect.Bottom);
 
-            dg.DrawLine(rect.X, iy, rect.X, iy2);
-            dg.DrawLine(rect.Right, iy, rect.Right, iy2);
+            dg.DrawLine(rect.X, rect.Y+lt, rect.X, rect.Bottom-lb);
+            dg.DrawLine(rect.Right, rect.Y+rt, rect.Right, rect.Bottom-rb);
 
             return dg;
         }
